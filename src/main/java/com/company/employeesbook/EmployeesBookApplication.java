9e04f9d7b685c4c80aba0e28ec1e8f0eb3fbaf0a
AleksandrVavilov <vavilov3388@gmail.com>
2022-10-20 13:15:50 +0300

    Initial commit

---
+++ src/main/java/com/company/employeesbook/EmployeesBookApplication.java
@@ -0,0 +1,13 @@
+package com.company.employeesbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeesBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/employeesbook/EmployeesBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.employeesbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
